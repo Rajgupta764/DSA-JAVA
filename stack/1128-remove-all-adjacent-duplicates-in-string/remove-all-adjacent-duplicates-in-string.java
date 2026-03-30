8702e35c0d19a3985ea0d672071588438c177f5a
Raj kumar <raj.23jics087@jietjodhpur.ac.in>
2026-03-31 00:28:30 +0530

    Time: 25 ms (57.09%) | Memory: 47.1 MB (59.06%) - LeetSync

---
+++ stack/1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> st=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(!st.isEmpty() && st.peek()==ch){
+                st.pop();
+            }
+            else {
+                st.push(ch);
+            }
+        }
+        StringBuilder result=new StringBuilder();
+        for(char ch:st){
+            result.append(ch);
+        }
+        return result.toString();
+    }
+}
