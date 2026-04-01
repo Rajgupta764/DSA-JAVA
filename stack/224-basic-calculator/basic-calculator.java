8307c40f53cbb70ad3e6403aaa39dc4ca759de57
Raj kumar <raj.23jics087@jietjodhpur.ac.in>
2026-04-01 22:03:49 +0530

    Time: 10 ms (86.12%) | Memory: 46.6 MB (46.18%) - LeetSync

---
+++ stack/224-basic-calculator/basic-calculator.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int calculate(String s) {
+        int n=s.length();
+        int number=0;
+        int result=0;
+        int sign=1;
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(Character.isDigit(ch)){
+                number=number*10+(ch-'0');
+            }
+            else if(ch=='+'){
+                 result+=number*sign;
+                 number=0;
+                 sign=1;
+            }
+            else if(ch=='-'){
+                   result+=number*sign;
+                   number=0;
+                   sign=-1;
+            }
+            else if(ch=='('){
+                  st.push(result);
+                  st.push(sign);
+                  result=0;
+                  sign=1;
+            }
+            else if(ch==')'){
+                 result+=(number*sign);
+                 number=0;
+                 int st_sign=st.pop(); 
+                 int st_result=st.pop();
+
+                 result=st_result+(st_sign*result);
+            }
+            else if(ch==' '){
+                continue;
+            }
+        }
+        return result+number*sign;
+    }
+}
