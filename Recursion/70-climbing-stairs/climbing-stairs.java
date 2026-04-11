7219c849c7c00a62111b80b287d26215b39523e4
Raj kumar <raj.23jics087@jietjodhpur.ac.in>
2026-04-11 19:46:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (23.37%) - LeetSync

---
+++ Recursion/70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    int[] memo;
+    public int climbStairs(int n) {
+        memo=new int[n+1];
+        return helper(n);
+    }
+    private int helper(int n){
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        if(memo[n]!=0){
+            return memo[n];
+        }
+        memo[n]=helper(n-1)+helper(n-2);
+        return memo[n];
+    }
+}
