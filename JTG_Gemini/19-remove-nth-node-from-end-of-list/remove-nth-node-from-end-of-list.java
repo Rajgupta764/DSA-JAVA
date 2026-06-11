b7fbf2549d3d8c0b6554d3e8cf6bd4cc53c676e0
Raj kumar <raj.23jics087@jietjodhpur.ac.in>
2026-06-11 09:05:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (6.26%) - LeetSync

--- JTG_Gemini/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ JTG_Gemini/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -12,8 +12,11 @@ class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode slow=head;
         ListNode fast=head;
+        if(head==null || head.next==null){
+            return null;
+        }
         for(int i=0;i<n;i++){
-            fast=fast.next;
+              fast=fast.next;
         }
         if(fast==null){
             return head.next;
