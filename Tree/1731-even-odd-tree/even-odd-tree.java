b06ff04d63f604c73bf9c78b61ad95080139ba16
Raj kumar <raj.23jics087@jietjodhpur.ac.in>
2026-06-25 09:10:35 +0530

    Time: 10 ms (86.12%) | Memory: 77.8 MB (61.15%) - LeetSync

--- Tree/1731-even-odd-tree/even-odd-tree.java
+++ Tree/1731-even-odd-tree/even-odd-tree.java
@@ -1,24 +1,34 @@
 class Solution {
     public boolean isEvenOddTree(TreeNode root) {
+        return check(root);
+    }
+    public boolean check(TreeNode root){
         Queue<TreeNode> pq=new LinkedList<>();
-        boolean even=true;
+        boolean isEven=true;
         pq.add(root);
         while(!pq.isEmpty()){
-            int prev=(even)?Integer.MIN_VALUE:Integer.MAX_VALUE;
+            int prev=(isEven)?Integer.MIN_VALUE:Integer.MAX_VALUE;
             int size=pq.size();
             for(int i=0;i<size;i++){
                 TreeNode current=pq.poll();
                 int values=current.val;
-                if((even && (values%2==0 || values<=prev)) || (!even && (values%2!=0 || values>=prev))){
+                if((isEven && (values%2==0 || values<=prev)) || (!isEven && (values%2!=0 || values>=prev))){
                     return false;
                 }
                 prev=values;
-                if(current.left!=null) pq.add(current.left);
-                if(current.right!=null) pq.add(current.right);
+                if(current.left!=null){
+                    pq.add(current.left);
+                }
+                if(current.right!=null){
+                    pq.add(current.right);
+                }
                 
             }
-            even=!even;
+            isEven=!isEven;
+            
         }
+
         return true;
+        
     }
 }
