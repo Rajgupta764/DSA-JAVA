b14786ce655335a1898c6de8e62f6d7c806cdae0
Raj kumar <raj.23jics087@jietjodhpur.ac.in>
2026-06-27 08:41:24 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (90.92%) - LeetSync

--- Tree/623-add-one-row-to-tree/add-one-row-to-tree.java
+++ Tree/623-add-one-row-to-tree/add-one-row-to-tree.java
@@ -8,24 +8,33 @@ class Solution {
             return newRoot;
         }
 
-        dfs(root,1,depth,val);
+        Queue<TreeNode> pq=new LinkedList<>();
+        pq.add(root);
+        int currDepth=1;
+        while(!pq.isEmpty()){
+            int size=pq.size();
+            for(int i=0;i<size;i++){
+                TreeNode current=pq.poll();
+                if(currDepth==depth-1){
+                    TreeNode newLeft=new TreeNode(val);
+                    newLeft.left=current.left;
+                    current.left=newLeft;
+                    TreeNode newRight=new TreeNode(val);
+                    newRight.right=current.right;
+                    current.right=newRight;
+
+                } else{
+                    if(current.left!=null){
+                        pq.add(current.left);
+                    }
+                    if(current.right!=null){
+                        pq.add(current.right);
+                    }
+                }
+                
+            }
+            currDepth++;
+        }
         return root;
-        
-    }
-    public void dfs(TreeNode root,int currDepth,int depth,int val){
-        if(root==null){
-            return;
-        }
-        if(currDepth==depth-1){
-            TreeNode newLeft=new TreeNode(val);
-            newLeft.left=root.left;
-            root.left=newLeft;
-            TreeNode newRight=new TreeNode(val);
-            newRight.right=root.right;
-            root.right=newRight;
-            return;
-        }
-        dfs(root.left,currDepth+1,depth,val);
-        dfs(root.right,currDepth+1,depth,val);
     }
 }
