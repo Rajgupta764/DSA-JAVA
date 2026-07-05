a5d71de92885560d738b98055e57b1931286c0a0
Raj kumar <raj.23jics087@jietjodhpur.ac.in>
2026-07-05 22:13:16 +0530

    Time: 1 ms (98.80%) | Memory: 46.4 MB (93.49%) - LeetSync

---
+++ Tree/1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.java
@@ -0,0 +1,37 @@
+
+class Solution {
+    
+    public TreeNode dfs(TreeNode root,HashSet<Integer> to_delete,List<TreeNode> forest){
+        if(root==null){
+            return root;
+        }
+        root.left=dfs(root.left,to_delete,forest);
+        root.right=dfs(root.right,to_delete,forest);
+        //process the root
+        if(!to_delete.contains(root.val)){
+            return root;
+        }
+        if(root.left!=null){
+            forest.add(root.left);
+        }
+        if(root.right!=null){
+            forest.add(root.right);
+        }
+        root.left=null;
+        root.right=null;
+        return null;
+    }
+
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int del:to_delete){
+            set.add(del);
+        }
+        List<TreeNode> forest=new ArrayList<>();
+        root=dfs(root,set,forest);
+        if(root!=null){
+            forest.add(root);
+        }
+        return forest; 
+    }
+}
